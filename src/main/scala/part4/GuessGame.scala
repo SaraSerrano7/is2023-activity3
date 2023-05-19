2eb0fdf5bb9ccdf7705f6ed2bbd5e7303e6a2c4b
SaraSerrano7 <sasecu@gmail.com>
2023-05-19 17:18:13 +0200

    part4 v3

--- src/main/scala/part4/GuessGame.scala
+++ src/main/scala/part4/GuessGame.scala
@@ -24,7 +24,9 @@ object GuessGame extends ZIOAppDefault:
     for 
       line <- Console.readLine(s"Enter a number between 1 and $maximum: ").orDie
       number <- ZIO.attempt(line.toInt).catchAll{case _: NumberFormatException => guessNumber(maximum, secret)}
-      _ <- if number < secret then
+      _ <- if number < 1  || number > maximum then
+        guessNumber(maximum, secret)
+        else if number < secret then
         badResult("target is HIGHER", maximum, secret)
         else if number > secret then
           badResult("target is LOWER", maximum, secret)
@@ -32,56 +34,24 @@ object GuessGame extends ZIOAppDefault:
           printMessage("Congratulations, you have guessed OK")
     yield (number)
 
+  def badResult(message: String, maximum: Int, secret: Int): ZIO[Any, Nothing, Unit] =
+    for 
+      _ <- printMessage(message)
+      _ <- guessNumber(maximum, secret)
+    yield()
 
+    
   def printMessage(message: String): ZIO[Any, Nothing, Unit] =
     for 
       _ <- Console.printLine(s"$message").orDie
     yield()
-    
-
-  def badResult(message: String, maximum: Int, secret: Int): ZIO[Any, Nothing, Unit] =
-    for 
-      // _ <- Console.printLine(s"$message").orDie
-      _ <- printMessage(message)
-      // _ <- readPossibleNum(secret)
-      _ <- guessNumber(maximum, secret)
-    yield()
-
-
-  // def readPossibleNum(secret: Int): ZIO[Any, Nothing, Unit] =
-  //   for
-  //     number <- readNumber 
-  //     result <- 
-  //       if number < secret then
-  //         badResult("target is HIGHER", secret)
-  //       else if number > secret then
-  //         badResult("target is LOWER", secret)
-  //       else
-  //         printMessage("Congratulations, you have guessed OK")
-  //   yield ()
 
   val program: ZIO[Any, Nothing, Unit] = 
     for 
       maxNum <- readNumber
-      secret <- Random.nextIntBounded(maxNum)
-      // _ <- readPossibleNum(secret)
+      secret <- Random.nextIntBetween(1, maxNum)//Random.nextIntBounded(maxNum)
+      _ <- printMessage(s"secret: $secret, maximum: $maxNum")
       _ <- guessNumber(maxNum, secret)
     yield ()
-
-
-
-
-  /*
-  1. Pedir un número al usuario
-    1a. Si no es numero, volver a pedirlo (como la primera implementacion que hice)
-  2. Calcular un numero random secreto entre 0 y el del usuario
-  3. Pedir un numero al usuario entre 0 y su maximo
-    3a. Si está out of scope, es una string, pedir otro
-    3b. Sino, si es diferente del secreto:
-      3ba. Si es mas grande, indicarlo
-      3bb. Si es mas pequeño, indicarlo
-    3c. Si es igual, indicar que ha ganado
-  GOTO 3
-  */
 
   val run = program
